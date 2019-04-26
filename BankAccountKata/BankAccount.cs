a7df72750cb05ef256570055a22a7e8419d33172
Mathieu <mathieu.racassin@lacombedulionvert.fr>
2019-04-26 15:40:12 +0200

    create bank account case

---
+++ BankAccountKata/BankAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAccountKata
+{
+    public class BankAccount
+    {
+        public double GetAmount()
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ BankAccountKataTest/ClientOperationTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BankAccountKata;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankAccountKataTest
+{
+    [TestClass]
+    public class ClientOperationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BankAccount bankAccount = new BankAccount();
+
+            double amount = bankAccount.GetAmount();
+
+            amount.Should().Be(0);
+        }
+    }
+}
